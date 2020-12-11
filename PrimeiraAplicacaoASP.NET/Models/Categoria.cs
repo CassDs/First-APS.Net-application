cf93c1da04af9d9bd934815e59e2810cd35d3a91
Cassio Dos Santos <cassioms764@gmail.com>
2020-12-11 12:56:03 -0300

    Criando class Categoria e suas prioridades

---
+++ PrimeiraAplicacaoASP.NET/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeiraAplicacaoASP.NET.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; } //Chave primaria das tabelas do banco de dados
+
+        public string Descricao { get; set; } //Descrição dos itens
+    }
+}
